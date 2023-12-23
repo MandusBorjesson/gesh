bbc2f44d7c5346d3ef6c17f842cd709b16f3fe93
Mandus Börjesson <borjessonmandus@gmail.com>
2023-12-23 19:33:47 +0100

    Add time printouts
    Very useful for performance testing

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 #include <chrono>
 #include <csignal>
+#include <iomanip>
 
 constexpr auto DBUS_SERVICE = "owl.gesh";
 constexpr auto DBUS_PATH = "/owl/gesh";
@@ -74,8 +75,37 @@ public:
 private:
 };
 
+class TimeKeeper {
+public:
+    TimeKeeper() {
+        m_start = std::chrono::steady_clock::now();
+        m_last = m_start;
+    };
+    void setLast(std::chrono::time_point<std::chrono::steady_clock> time) { m_last = time; };
+    friend std::ostream& operator<<(std::ostream& os, TimeKeeper& t) {
+        auto t_now = std::chrono::steady_clock::now();
+        const std::chrono::duration diff_start = t_now - t.m_start;
+        const std::chrono::duration diff_last = t_now - t.m_last;
+
+        double diff_start_double = 1000 * double(diff_start.count()) * std::chrono::steady_clock::period::num / std::chrono::steady_clock::period::den;
+        double diff_last_double = 1000* double(diff_last.count()) * std::chrono::steady_clock::period::num / std::chrono::steady_clock::period::den;
+;
+
+        os << std::fixed << std::setprecision(2) << std::left;
+        std::cout << "Time since start: " << diff_start_double << "ms, Delta time: " << diff_last_double << "ms.";
+        t.setLast(t_now);
+        return os;
+    };
+
+protected:
+    std::chrono::time_point<std::chrono::steady_clock> m_start;
+    std::chrono::time_point<std::chrono::steady_clock> m_last;
+};
+
 int main()
 {
+    auto t_keeper = TimeKeeper();
+
     signal(SIGINT, signalHandler);
 
     INFO << "Gesh - Good enough setting handler" << std::endl;
@@ -84,6 +114,7 @@ int main()
 
     auto init = SettingInitializerHardcoded();
     std::vector<ISettingInitializer> extraSource;
+    DEBUG << "Setting initialization DONE. " << t_keeper << std::endl;
 
     auto handler = SettingHandler(init, extraSource);
     for ( auto const& [key, val] : handler.Settings() ) {
@@ -93,9 +124,11 @@ int main()
     auto connection = sdbus::createSessionBusConnection();
     connection->requestName(DBUS_SERVICE);
     connection->enterEventLoopAsync();
+    DEBUG << "D-Bus service name aquired. " << t_keeper << std::endl;
 
     auto manager = std::make_unique<ManagerAdaptor>(*connection, DBUS_PATH);
     auto settingManager = std::make_unique<DbusAdaptor>(*connection, DBUS_PATH);
+    DEBUG << "D-Bus objects registered. " << t_keeper << std::endl;
 
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
