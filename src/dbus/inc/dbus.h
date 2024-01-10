0f5f1337c85b9247bc332717b7a77eae41aea1d1
Mandus Börjesson <borjessonmandus@gmail.com>
2024-01-10 21:21:49 +0100

    Clean up source structure
    Things were getting a bit flat and out of hand, restructure a bit to make it
    more sane.

---
+++ src/dbus/inc/dbus.h
@@ -0,0 +1,51 @@
+#include "dbus-abstraction.h"
+#include "setting.h"
+#include <sdbus-c++/sdbus-c++.h>
+
+class ManagerAdaptor : public sdbus::AdaptorInterfaces< sdbus::ObjectManager_adaptor >
+{
+public:
+    ManagerAdaptor(sdbus::IConnection& connection, std::string path)
+    : AdaptorInterfaces(connection, std::move(path))
+    {
+        registerAdaptor();
+    }
+
+    ~ManagerAdaptor()
+    {
+        unregisterAdaptor();
+    }
+};
+
+class DbusAdaptor final : public sdbus::AdaptorInterfaces< owl::gesh::setting_adaptor,
+                                                sdbus::ManagedObject_adaptor,
+                                                sdbus::Properties_adaptor >
+{
+public:
+    DbusAdaptor(sdbus::IConnection& connection, std::string path, SettingHandler *handler)
+    : AdaptorInterfaces(connection, std::move(path)), m_handler(handler)
+    {
+        registerAdaptor();
+        emitInterfacesAddedSignal({owl::gesh::setting_adaptor::INTERFACE_NAME});
+    }
+
+    ~DbusAdaptor()
+    {
+        emitInterfacesRemovedSignal({owl::gesh::setting_adaptor::INTERFACE_NAME});
+        unregisterAdaptor();
+    }
+
+    void UpdateRequiresReset(const std::string& name) override
+    {
+        return;
+    }
+
+    std::vector<sdbus::Variant> Get(const std::vector<std::string>& names) override;
+    std::map<std::string, sdbus::Variant> GetAll() override;
+    void Set(const std::map<std::string, sdbus::Variant>& settings);
+
+private:
+    sdbus::Variant ToSdBusVariant(const setting_t &val);
+    std::string ToString(const sdbus::Variant &val);
+    SettingHandler *m_handler;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#include "dbus-abstraction.h"
+#include "dbus.h"
 #include "setting.h"
 #include "settingInitializerHardcoded.h"
 #include "settingReaderFile.h"
-#include "settingTypes.h"
 #include <sdbus-c++/sdbus-c++.h>
 #include "log.h"
 #include "version.h"
@@ -23,114 +22,6 @@ void signalHandler( int signum ) {
 
    exit(signum);
 }
-
-class ManagerAdaptor : public sdbus::AdaptorInterfaces< sdbus::ObjectManager_adaptor >
-{
-public:
-    ManagerAdaptor(sdbus::IConnection& connection, std::string path)
-    : AdaptorInterfaces(connection, std::move(path))
-    {
-        registerAdaptor();
-    }
-
-    ~ManagerAdaptor()
-    {
-        unregisterAdaptor();
-    }
-};
-
-class DbusAdaptor final : public sdbus::AdaptorInterfaces< owl::gesh::setting_adaptor,
-                                                sdbus::ManagedObject_adaptor,
-                                                sdbus::Properties_adaptor >
-{
-public:
-    DbusAdaptor(sdbus::IConnection& connection, std::string path, SettingHandler *handler)
-    : AdaptorInterfaces(connection, std::move(path)), m_handler(handler)
-    {
-        registerAdaptor();
-        emitInterfacesAddedSignal({owl::gesh::setting_adaptor::INTERFACE_NAME});
-    }
-
-    ~DbusAdaptor()
-    {
-        emitInterfacesRemovedSignal({owl::gesh::setting_adaptor::INTERFACE_NAME});
-        unregisterAdaptor();
-    }
-
-    void UpdateRequiresReset(const std::string& name) override
-    {
-        return;
-    }
-
-    std::vector<sdbus::Variant> Get(const std::vector<std::string>& names) override {
-
-        std::vector<setting_t> settings;
-        try {
-            settings = m_handler->Get(names);
-        } catch ( SettingException const & ex ) {
-            std::string err = ex.what();
-            ERROR << "Failed to get setting(s): " + err << std::endl;;
-            throw sdbus::Error("own.gesh.Error", "Failed to get setting(s): " + err);
-        }
-        auto out = std::vector<sdbus::Variant>();
-        for (auto setting: settings) {
-            out.push_back(sdbus::Variant(ToSdBusVariant(setting)));
-        }
-        return out;
-    }
-
-    std::map<std::string, sdbus::Variant> GetAll() override {
-        auto out = std::map<std::string, sdbus::Variant>();
-        for (auto setting: m_handler->GetAll()) {
-            out[setting.first] = ToSdBusVariant(setting.second.Get());
-        }
-        return out;
-    }
-
-    void Set(const std::map<std::string, sdbus::Variant>& settings) {
-        DEBUG << "Set called" << std::endl;
-        std::map<std::string, std::string> in;
-        for (auto const& [key, val] : settings) {
-            in[key] = std::string(ToString(val));
-        }
-        try {
-            m_handler->Set(in);
-        } catch ( SettingException const & ex ) {
-            std::string err = ex.what();
-            ERROR << "Failed to set setting(s): " + err << std::endl;;
-            throw sdbus::Error("own.gesh.Error", "Failed to set setting(s): " + err);
-        }
-        emitSettingsUpdated(settings);
-    }
-
-private:
-    sdbus::Variant ToSdBusVariant(const setting_t &val) {
-        if (std::holds_alternative<std::string>(val)) {
-            return sdbus::Variant(std::get<std::string>(val));
-        } else if (std::holds_alternative<int>(val)) {
-            return sdbus::Variant(std::get<int>(val));
-        } else if (std::holds_alternative<bool>(val)) {
-            return sdbus::Variant(std::get<bool>(val));
-        } else {
-            return sdbus::Variant(NULL);
-        }
-    }
-    std::string ToString(const sdbus::Variant &val) {
-        auto type = val.peekValueType();
-        if (type == "s") {
-            return std::string(val);
-        } else if (type == "i") {
-            return std::to_string(int(val));
-        } else if (type == "b") {
-            return (bool(val) == true) ? "1" : "0";
-        } else {
-            auto err = "Unknown variant type '" + type + "'";
-            ERROR << err << std::endl;
-            throw sdbus::Error("own.gesh.Error", err);
-        }
-    }
-    SettingHandler *m_handler;
-};
 
 class TimeKeeper {
 public:
