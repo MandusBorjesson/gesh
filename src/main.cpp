2439f735362b9f142dece5fcb9f832c693477127
Mandus Börjesson <borjessonmandus@gmail.com>
2024-01-18 21:15:03 +0100

    main: Remove time profiling
    While sub-ms profiling is cool, it wasnt really relevant. If gesh starts within
    a second that is good enough, chasing milliseconds goes against the 'good
    enough' philosophy.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include "version.h"
 #include <memory>
 #include <thread>
-#include <chrono>
 #include <csignal>
 #include <iomanip>
 
@@ -23,33 +22,6 @@ void signalHandler( int signum ) {
    exit(signum);
 }
 
-class TimeKeeper {
-public:
-    TimeKeeper() {
-        m_start = std::chrono::steady_clock::now();
-        m_last = m_start;
-    };
-    void setLast(std::chrono::time_point<std::chrono::steady_clock> time) { m_last = time; };
-    friend std::ostream& operator<<(std::ostream& os, TimeKeeper& t) {
-        auto t_now = std::chrono::steady_clock::now();
-        const std::chrono::duration diff_start = t_now - t.m_start;
-        const std::chrono::duration diff_last = t_now - t.m_last;
-
-        double diff_start_double = 1000 * double(diff_start.count()) * std::chrono::steady_clock::period::num / std::chrono::steady_clock::period::den;
-        double diff_last_double = 1000* double(diff_last.count()) * std::chrono::steady_clock::period::num / std::chrono::steady_clock::period::den;
-;
-
-        os << std::fixed << std::setprecision(2) << std::left;
-        std::cout << "Time since start: " << diff_start_double << "ms, Delta time: " << diff_last_double << "ms.";
-        t.setLast(t_now);
-        return os;
-    };
-
-protected:
-    std::chrono::time_point<std::chrono::steady_clock> m_start;
-    std::chrono::time_point<std::chrono::steady_clock> m_last;
-};
-
 void print_help() {
     INFO << std::endl;
     INFO << "Usage:" << std::endl;
@@ -64,8 +36,6 @@ void print_help() {
 
 int main(int argc, char *argv[])
 {
-    auto t_keeper = TimeKeeper();
-
     signal(SIGINT, signalHandler);
 
     INFO << "Gesh - Good enough setting handler" << std::endl;
@@ -102,9 +72,9 @@ int main(int argc, char *argv[])
     auto srf = SettingReaderFactory(searchPaths);
     std::vector<ISettingReader*> readers = srf.getReaders();
 
-    DEBUG << "Initializing settings... " << t_keeper << std::endl;
+    DEBUG << "Initializing settings... " << std::endl;
     auto handler = SettingHandler(init, readers);
-    DEBUG << "Setting initialization DONE. " << t_keeper << std::endl;
+    DEBUG << "Setting initialization DONE. " << std::endl;
     for ( auto const& [key, val] : handler.GetAll() ) {
         DEBUG << val << std::endl;
     }
@@ -112,11 +82,11 @@ int main(int argc, char *argv[])
     auto connection = sdbus::createSessionBusConnection();
     connection->requestName(DBUS_SERVICE);
     connection->enterEventLoopAsync();
-    DEBUG << "D-Bus service name aquired. " << t_keeper << std::endl;
+    DEBUG << "D-Bus service name aquired. " << std::endl;
 
     auto manager = std::make_unique<ManagerAdaptor>(*connection, DBUS_PATH);
     auto settingManager = std::make_unique<DbusAdaptor>(*connection, DBUS_PATH, &handler);
-    DEBUG << "D-Bus objects registered. " << t_keeper << std::endl;
+    DEBUG << "D-Bus objects registered. " << std::endl;
 
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
