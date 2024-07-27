7ae4ff147948051e74f3cc760b5e70dbfe6b1c3b
Mandus Börjesson <borjessonmandus@gmail.com>
2024-07-27 09:20:33 +0200

    Const-ify logging helper class

--- src/common/inc/log.h
+++ src/common/inc/log.h
@@ -42,7 +42,7 @@ std::string getColor(LogLevel level) {
 
 class JournalLog {
 public:
-    JournalLog(std::string &name, LogLevel level) {
+    JournalLog(const std::string &name, LogLevel level) {
         operator << (getColor(level) + (level == NONE ? "" : "<"+std::to_string(level)+"> ") +
                     (name.empty() ? "" : "["+name+"] "));
     }
@@ -67,15 +67,15 @@ public:
 
     Log getChild(std::string name)  { return Log(m_name + "." + name); };
 
-    JournalLog fatal()    { return JournalLog(m_name, FATAL); };
-    JournalLog alert()    { return JournalLog(m_name, ALERT); };
-    JournalLog critical() { return JournalLog(m_name, CRITICAL); };
-    JournalLog error()    { return JournalLog(m_name, ERROR); };
-    JournalLog warning()  { return JournalLog(m_name, WARNING); };
-    JournalLog notice()   { return JournalLog(m_name, NOTICE); };
-    JournalLog info()     { return JournalLog(m_name, INFO); };
-    JournalLog debug()    { return JournalLog(m_name, DEBUG); };
-    JournalLog none()     { return JournalLog(m_name, NONE); };
+    JournalLog fatal() const { return JournalLog(m_name, FATAL); };
+    JournalLog alert() const { return JournalLog(m_name, ALERT); };
+    JournalLog critical() const { return JournalLog(m_name, CRITICAL); };
+    JournalLog error() const { return JournalLog(m_name, ERROR); };
+    JournalLog warning() const { return JournalLog(m_name, WARNING); };
+    JournalLog notice() const { return JournalLog(m_name, NOTICE); };
+    JournalLog info() const { return JournalLog(m_name, INFO); };
+    JournalLog debug() const { return JournalLog(m_name, DEBUG); };
+    JournalLog none() const { return JournalLog(m_name, NONE); };
 private:
     std::string m_name;
 };
